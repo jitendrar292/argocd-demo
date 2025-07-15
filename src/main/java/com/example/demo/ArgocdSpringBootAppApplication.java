6556289188aa5adfacec4309c895dbcda4a8bdeb
unknown <jitendrar292@gmail.com>
2025-07-15 16:39:51 +0530

    initial commit

---
+++ src/main/java/com/example/demo/ArgocdSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgocdSpringBootAppApplication.class, args);
+	}
+
+}
